5550dcd1509453fc4b5b49fb10d5604344945067
Andreas Riemer <andreas.riemer@gmail.com>
2010-03-04 15:48:16 +0100

    CHANGED JsonTransport and RackProvider can handle exceptions now. CHANGED Prepared handling of http error codes, too. ADDED Started an exception hierarchy.

--- lib/hoth.rb
+++ lib/hoth.rb
@@ -17,5 +17,6 @@ require 'hoth/services'
 require 'hoth/util/logger'
 
 require 'hoth/extension/core/exception'
+require 'hoth/exceptions'
 
 Hoth::Logger.init_logging!
---
+++ lib/hoth/exceptions.rb
@@ -0,0 +1,13 @@
+module Hoth
+  class HothException < StandardError
+    attr_reader :original
+    def self.wrap(original)
+      wrapped = new("#{original.class} says: #{original.message}")
+      wrapped.set_backtrace original.backtrace
+      wrapped.instance_variable_set :@original, original
+      wrapped
+    end
+  end
+
+  class TransportError < HothException; end
+end
