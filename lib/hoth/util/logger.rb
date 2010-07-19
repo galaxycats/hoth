4bbe3c874293d6116fcd8dcb78f941bb22287bca
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2010-07-19 13:32:44 +0200

    made Hoth::Logger::log_provider not private anymore
    If you want to provide your own logger, it is
    much easier if you can access the logger (e.g. to
    set the log level).

--- lib/hoth/util/logger.rb
+++ lib/hoth/util/logger.rb
@@ -1,11 +1,11 @@
 module Hoth
   class Logger
     class <<self
-      
+
       def log_provider=(log_provider)
         @log_provider = log_provider
       end
-      
+
       def init_logging!(logfile="log/hoth.log")
         Hoth::Logger.log_provider = if Object.const_defined?("Rails")
           Rails.logger
@@ -14,33 +14,30 @@ module Hoth
           ::Logger.new(logfile)
         end
       end
-      
+
       def debug(msg)
         log_provider.debug msg
       end
-      
+
       def info(msg)
         log_provider.info msg
       end
-      
+
       def warn(msg)
         log_provider.warn msg
       end
-      
+
       def error(msg)
         log_provider.error msg
       end
-      
+
       def fatal(msg)
         log_provider.fatal msg
       end
-      
-      private
-      
-        def log_provider
-          @log_provider || init_logging!
-        end
+
+      def log_provider
+        @log_provider || init_logging!
+      end
     end
-        
   end
 end
