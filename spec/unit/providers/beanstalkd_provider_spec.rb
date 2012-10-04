4ccb07ae6128a8f65b22da7e7debe319f024bbc8
Dirk Breuer <dirk.breuer@gmail.com>
2012-10-04 12:26:34 +0200

    beanstalk transport will use just one connection per service

--- spec/unit/providers/beanstalkd_provider_spec.rb
+++ spec/unit/providers/beanstalkd_provider_spec.rb
@@ -2,12 +2,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../../',  'spec_help
 
 require File.expand_path(File.join(File.dirname(__FILE__), '../../../',  'lib', 'hoth', 'providers', 'beanstalkd_provider'))
 
-require 'rack/mock'
-
 module Hoth
   module Providers
     describe BeanstalkdProvider do
-      
+
       it "should know its module name" do
         # cannot be tested since i do not know how to mock constructor call
         # http://stackoverflow.com/questions/5270819/how-to-test-a-method-call-in-a-constructor-with-rspec
@@ -15,11 +13,11 @@ module Hoth
         # beanstalkd_provider = Hoth::Providers::BeanstalkdProvider.new("foo_module")
         # beanstalkd_provider.module_name.should == "foo_module"
       end
-      
+
       it "should throw an error if no module name is passed" do
         lambda { Hoth::Providers::BeanstalkdProvider.new }.should raise_error(ArgumentError)
       end
-      
+
       it "should only listen for services defined for the passed module name" do
         mail_module = Hoth::ServiceModule.new(:name => "mail_module")
         mail_module.should_not_receive(:registered_services)
@@ -31,7 +29,8 @@ module Hoth
           :mail_module => mail_module,
           :foo_module => foo_module
         })
-        beanstalkd_provider = Hoth::Providers::BeanstalkdProvider.new("foo_module")
+
+        Hoth::Providers::BeanstalkdProvider.new("foo_module")
       end
 
     end
--- spec/unit/transport/beanstalkd_spec.rb
+++ spec/unit/transport/beanstalkd_spec.rb
@@ -2,43 +2,34 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../../',  'spec_help
 
 module Hoth
   module Transport
-    
+
     describe "Beanstalkd" do
       before(:each) do
-          endpoint = mock("EndpointMock")
-          endpoint.stub(:host).and_return("localhost")
-          endpoint.stub(:port).and_return("11300")
+        endpoint = mock("EndpointMock")
+        endpoint.stub(:host).and_return("localhost")
+        endpoint.stub(:port).and_return("11300")
 
-          service_module = mock("ServiceModule")
-          service_module.should_receive(:name).and_return(:test_service_module)
+        service_module = mock("ServiceModule")
+        service_module.should_receive(:name).and_return(:test_service_module)
 
-          @service = mock("ServiceMock")
-          @service.should_receive(:name).and_return(:test_service)
-          @service.should_receive(:endpoint).any_number_of_times.and_return(endpoint)
-          @service.should_receive(:module).any_number_of_times.and_return(service_module)
+        @service = mock("ServiceMock")
+        @service.should_receive(:name).and_return(:test_service)
+        @service.should_receive(:endpoint).any_number_of_times.and_return(endpoint)
+        @service.should_receive(:module).any_number_of_times.and_return(service_module)
 
-          @beanstalk_connection = mock("Beanstalk::Connection")
+        @beanstalk_connection = mock("Beanstalk::Connection")
       end
 
-      it "should send the payload over a simple Beanstalk::Connection and close the connection afterwards" do
-        Beanstalk::Connection.should_receive(:new).with("localhost:11300").and_return(@beanstalk_connection)
-        @beanstalk_connection.should_receive(:use).with(anything)
+      it "should send the payload over an existing Beanstalk::Connection and don't close it afterwards" do
+        Beanstalk::Connection.should_receive(:new).with("localhost:11300").once.and_return(@beanstalk_connection)
+        @beanstalk_connection.should_receive(:use).with(anything).any_number_of_times
         @beanstalk_connection.should_receive(:put).with(["some_data_to_send"])
-        @beanstalk_connection.should_receive(:close)
+        @beanstalk_connection.should_receive(:put).with(["some_more_data_to_send"])
+        @beanstalk_connection.should_receive(:close).never
 
         transport = Beanstalkd.new(@service)
         transport.call_remote_with("some_data_to_send")
-      end
-
-      it "should ensure to close the connection after sending the payload to beanstalkd" do
-        Beanstalk::Connection.should_receive(:new).with("localhost:11300").and_return(@beanstalk_connection)
-        @beanstalk_connection.should_receive(:use).with(anything)
-        @beanstalk_connection.should_receive(:put).never
-        @beanstalk_connection.should_receive(:close)
-
-        transport = Beanstalkd.new(@service)
-        transport.encoder.should_receive(:encode).with(anything).and_raise("some error")
-        transport.call_remote_with("some_data_to_send")
+        transport.call_remote_with("some_more_data_to_send")
       end
 
       it "should use the beanstalk tube specified by the transport" do
@@ -46,7 +37,7 @@ module Hoth
         transport = Beanstalkd.new(@service)
         @beanstalk_connection.should_receive(:use).with(transport.tube_name)
         @beanstalk_connection.should_receive(:put).with(["some_data_to_send"])
-        @beanstalk_connection.should_receive(:close)
+        @beanstalk_connection.should_receive(:close).never
 
         transport.call_remote_with("some_data_to_send")
       end
